5d442930c930873411d78ac64bc9c6135a3208f9
PKyes44 <rugate23@gmail.com>
2024-09-30 10:40:27 +0900

    feat: button cva

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,12 +3,17 @@ import { cva, VariantProps } from "class-variance-authority";
 
 const buttonVariant = cva("", {
 	variants: {
-		outline: {
-			true: "border border-black",
-			false: "border-none",
+		size: {
+			sm: "py-2 px-8 text-sm",
+			md: "py-3 px-16",
+			lg: "py-4 px-24 text-lg",
 		},
 		intent: {
 			primary: "bg-black text-white",
+		},
+		outline: {
+			true: "border border-black",
+			false: "border-none",
 		},
 		rounded: {
 			none: "rounded-none",
@@ -19,6 +24,7 @@ const buttonVariant = cva("", {
 	},
 	compoundVariants: [{ outline: true, className: "text-black" }],
 	defaultVariants: {
+		size: "md",
 		intent: "primary",
 		outline: false,
 		rounded: "none",
@@ -28,9 +34,11 @@ const buttonVariant = cva("", {
 type ButtonVariants = VariantProps<typeof buttonVariant>;
 type ButtonProps = PropsWithChildren<ButtonVariants>;
 
-function Button({ outline, intent, rounded, children }: ButtonProps) {
+function Button({ size, outline, intent, rounded, children }: ButtonProps) {
 	return (
-		<button className={`${buttonVariant({ outline, intent, rounded })}`}>
+		<button
+			className={`${buttonVariant({ size, outline, intent, rounded })}`}
+		>
 			{children}
 		</button>
 	);
