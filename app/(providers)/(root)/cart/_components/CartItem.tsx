f9f1b92465cf148e2e9a50f9185e8bd2c36f7cff
PKyes44 <rugate23@gmail.com>
2024-10-01 13:37:42 +0900

    debug: cart style

--- app/(providers)/(root)/cart/_components/CartItem.tsx
+++ app/(providers)/(root)/cart/_components/CartItem.tsx
@@ -2,7 +2,6 @@
 
 import Button from "@/components/Button/Button";
 import { Cart } from "@/types/cart.type";
-import React from "react";
 import useCartItem from "./CartItem.hooks";
 
 interface CartItemProps {
@@ -16,25 +15,27 @@ function CartItem({ cartItem, invalidateQuery }: CartItemProps) {
 
 	return (
 		<article className="flex flex-row py-4 gap-x-16 justify-between">
-			<img
-				src={product.imgSrc}
-				alt="product image"
-				className="md:w-36 md:h-44"
-			/>
-			<div className="flex flex-col gap-y-3">
-				<span className="font-extrabold">
-					{product.brand.nameKr}/{product.brand.nameEn}
-				</span>
-				<span className="font-medium">{product.name}</span>
-				<div className="font-extrabold flex gap-x-4">
-					<span className="line-through text-red-500">
-						₩{product.originalPrice}
+			<div className="flex flex-row gap-x-4">
+				<img
+					src={product.imgSrc}
+					alt="product image"
+					className="md:w-36 md:h-44"
+				/>
+				<div className="flex flex-col gap-y-3">
+					<span className="font-extrabold">
+						{product.brand.nameKr}/{product.brand.nameEn}
 					</span>
-					<span className="font-extrabold">₩{product.price}</span>
-				</div>
-				<div className="text-sm">
-					<span>{product.deliveryType} </span>|
-					<span> 잔여재고 {product.onlineStock}ea</span>
+					<span className="font-medium">{product.name}</span>
+					<div className="font-extrabold flex gap-x-4">
+						<span className="line-through text-red-500">
+							₩{product.originalPrice}
+						</span>
+						<span className="font-extrabold">₩{product.price}</span>
+					</div>
+					<div className="text-sm">
+						<span>{product.deliveryType} </span>|
+						<span> 잔여재고 {product.onlineStock}ea</span>
+					</div>
 				</div>
 			</div>
 			<div className="flex items-center justify-end">
